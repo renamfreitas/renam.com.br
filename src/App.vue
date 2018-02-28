d536630aa0088c21858ef13ac6fa1e5cdbfe55a3
Renam Freitas <renamdefreitas@gmail.com>
2018-02-28 00:22:40 -0300

    basic spa with router and transition

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.svg">
+    <router-link v-bind:to="'/'">Home</router-link>
+    <router-link v-bind:to="'/about'">About</router-link>
+    <router-link v-bind:to="'/contact'">Contact</router-link>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  height: 80px;
+}
+</style>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+.child-view {
+  position: absolute;
+  transition: all .5s cubic-bezier(.55,0,.1,1);
+}
+.slide-left-enter, .slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(30px, 0);
+  transform: translate(30px, 0);
+}
+.slide-left-leave-active, .slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(-30px, 0);
+  transform: translate(-30px, 0);
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="contact">
+    <h1>{{ msg }}</h1>
+    <p>Contact me</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      msg: 'Contact me'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
